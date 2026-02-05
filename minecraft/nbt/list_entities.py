7d152c82b66ec8d3f2dee88e64d1d36f86b30f67
AshBill <me@ashbill.com>
2026-02-05 16:43:08 -0500

    🐛 再加个try catch

--- minecraft/nbt/list_entities.py
+++ minecraft/nbt/list_entities.py
@@ -4,7 +4,7 @@ import csv
 import anvil # pip install anvil-parser2
 
 
-root = pathlib.Path("D:\\Program Files\\PCL2\\.minecraft\\versions\\新岛铁道 v1.5.1 AshBill版\\saves\\create-cn_world_2026-01-25_DATA\\entities") # /path/to/entities
+root = pathlib.Path("entities") # /path/to/entities
 
 
 with open("entities.csv", "w", newline="", encoding="utf-8") as entities_file, \
@@ -15,23 +15,27 @@ with open("entities.csv", "w", newline="", encoding="utf-8") as entities_file, \
     chunks_writer.writerow(["chunk", "count"])
 
     for path in root.rglob("*.mca"):
-        region = anvil.Region.from_file(str(path))
-        for x in range(32):
-            for z in range(32):
-                try:
-                    nbt = region.chunk_data(x, z)
-                    if nbt is not None:
-                        entities = nbt["Entities"]
-                        chunks_writer.writerow([nbt["Position"].value, len(entities)])
-                        for entity in entities:
-                            uuid = 0
-                            for n in entity["UUID"].value:
-                                uuid = uuid << 32 | n & 0xFFFFFFFF
-                            uuid = f"{uuid:032x}"
-                            id = entity.get("id")
-                            pos = [d.value for d in entity.get("Pos")]
-                            print(uuid, id, pos)
-                            entities_writer.writerow([uuid, id] + pos)
+        try:
+            region = anvil.Region.from_file(str(path))
+            for x in range(32):
+                for z in range(32):
+                    try:
+                        nbt = region.chunk_data(x, z)
+                        if nbt is not None:
+                            entities = nbt["Entities"]
+                            chunks_writer.writerow([nbt["Position"].value, len(entities)])
+                            for entity in entities:
+                                uuid = 0
+                                for n in entity["UUID"].value:
+                                    uuid = uuid << 32 | n & 0xFFFFFFFF
+                                uuid = f"{uuid:032x}"
+                                id = entity.get("id")
+                                pos = [d.value for d in entity.get("Pos")]
+                                print(uuid, id, pos)
+                                entities_writer.writerow([uuid, id] + pos)
 
-                except IndexError:
-                    print("Failed to read region: ", path, (x, z))
+                    except IndexError:
+                        print("Failed to read region's chunk: ", path, (x, z))
+        except Exception as error:
+            print("Failed to read region file: ", path, error)
+            continue
